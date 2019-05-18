f1af2b14d1ebd3197bfb485c0b86f48c56c1403e
Daniel <daniel.shalom@slatescience.com>
2019-05-18 21:01:16 +0300

    new project

--- src/main/java/PageObjects/MatificHomePageObjects.java
+++ src/main/java/PageObjects/MatificHomePageObjects.java
@@ -1,4 +1,5 @@
 package PageObjects;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,28 +16,26 @@ public class MatificHomePageObjects {
     By NextButton = By.id("login-text");
 
 
-    public MatificHomePageObjects(WebDriver driver) {this.driver = driver; }
+    public MatificHomePageObjects(WebDriver driver) {
+        this.driver = driver;
+    }
 
     //Basic Functions
-    public void ClickMainLoginButton(){ driver.findElement(LoginButton).click(); }
-    public void NextButtonClick() {driver.findElement(NextButton).click();   }
-    public void TypeUserName(String value){
+    public void ClickMainLoginButton() {
+        driver.findElement(LoginButton).click();
+    }
+
+    public void NextButtonClick() {
+        driver.findElement(NextButton).click();
+    }
+
+    public void TypeUserName(String value) {
         driver.findElement(Username).sendKeys(value);
     }
-    public void TypePassword(String value){
+
+    public void TypePassword(String value) {
         driver.findElement(Password).sendKeys(value);
     }
 
-
-
-  /*WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='/isr/he/login-page/")));
-        loginButton.click();
-        WebElement UsernameField = wait.until(ExpectedConditions.elementToBeClickable(By.id("username-input")));
-        UsernameField.sendKeys("afteacher");
-        WebElement NextButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("login-text")));
-        NextButton.click();
-        WebElement PasswordField = wait.until(ExpectedConditions.elementToBeClickable(By.id("password-input")));
-        PasswordField.sendKeys("123");
-        NextButton.click();               */
 
 }
--- src/main/java/smartWait.java
+++ src/main/java/smartWait.java
@@ -2,14 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class smartWait {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver(); // Open Chrome
-        WebDriverWait wait = new WebDriverWait(driver,60);  //fluent wait
+        WebDriverWait wait = new WebDriverWait(driver, 60);  //fluent wait
         driver.manage().window().maximize();
         driver.get("https://practis.co.il/automation/whysync.html");
         WebElement button2 = wait.until(ExpectedConditions.elementToBeClickable(By.id("btn1")));
